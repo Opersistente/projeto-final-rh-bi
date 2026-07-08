1ac41aa22ca7bf8ebdb10d6eb07e54e9dc21d279
Luis Oliveira <luisoliveira.bb@gmail.com>
2026-07-07 21:09:35 -0300

    implementa consultas SQL e exportação CSV

---
+++ sql/sql_2.sql
@@ -0,0 +1,32 @@
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    e.salary,
+    d.department_name,
+    l.city,
+    l.state_province,
+    c.country_name,
+    r.region_name
+
+FROM HR.EMPLOYEES e
+
+LEFT JOIN HR.DEPARTMENTS d
+ON e.department_id = d.department_id
+
+LEFT JOIN HR.LOCATIONS l
+ON d.location_id = l.location_id
+
+LEFT JOIN HR.COUNTRIES c
+ON l.country_id = c.country_id
+
+LEFT JOIN HR.REGIONS r
+ON c.region_id = r.region_id
+
+WHERE r.region_name IS NOT NULL
+
+ORDER BY
+    r.region_name,
+    c.country_name,
+    l.city,
+    e.salary DESC;
